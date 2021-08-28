5bea75bc1c1d49ac8e25a1e61ee28d10bb400403
faizalkureshi11 <faizal.kureshi11@gmail.com>
2021-08-28 19:43:12 +0530

    forth commit

--- calculator_test.py
+++ calculator_test.py
@@ -1,4 +1,4 @@
-#TASK-2
+#TASK-3
 import pytest
 
 def test():
@@ -15,6 +15,10 @@ def test():
     assert(add("2,3,4,5") == 14)
     assert(add("1,2,3,6,8") == 20)
     assert(add("2,3,7,7,6,9") == 34)
+    # Handle New Lines
+    assert(add("1\n2,3") == 6)
+    assert(add("9\n6,5\n4") == 24)
+    assert(add("4\n3\n6\n1") == 14)
 
 def add(numbersString):
     if len(numbersString) == 0:
@@ -22,10 +26,12 @@ def add(numbersString):
     elif len(numbersString) == 1:
         return int(numbersString)
     else:
-        result = 0 
+        numbersString = numbersString.replace("\n",",")
+        result = 0
         ans = numbersString.split(",")
         for num in ans:
             result += int(num)
         return result
         
 test()
+
